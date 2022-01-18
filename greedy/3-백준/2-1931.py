3ae7a18279cbdc21e87d479ff0d9a0d7a443a862
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-18 20:29:26 +0900

    update : another way to solve a problem

--- greedy/3-백준/2-1931.py
+++ greedy/3-백준/2-1931.py
@@ -1,3 +1,5 @@
+
+# python 파일 입출력
 # https://velog.io/@yeseolee/Python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%A0%A5-%EC%A0%95%EB%A6%ACsys.stdin.readline
 import sys
 input = sys.stdin.readline
@@ -13,9 +15,9 @@ select_list = [time_list[0]]
 for time in time_list[1:]:
     # 해당 조건식에서 start == end 인 경우는 elif 로 넘어가도록 부등호를 잘 확인해야한다
     # 결국 문제에 있는 조건을 잘 읽어라
-    if (select_list[-1][0] <= time[0]) and (time[1] < select_list[-1][1]):
+    if time[1] < select_list[-1][1]:
         select_list[-1] = time
-    elif (time[0] >= select_list[-1][1]) and (time[1] >= select_list[-1][1]):
+    elif time[0] >= select_list[-1][1]:
         select_list.append(time)
 
 print(len(select_list))
@@ -28,3 +30,17 @@ print(len(select_list))
 # 다른 풀이 방법
 # end time을 기준으로 정렬을 한다
 # https://www.acmicpc.net/source/37732323
+
+N = int(input())
+time_list = []
+for i in range(N):
+    time_list.append(list(map(int, input().split())))
+time_list.sort(key=lambda x: (x[1], x[0]))
+
+end_time = time_list[0][1]
+count = 1
+for time in time_list[1:]:
+    if end_time <= time[0]:
+        end_time = time[1]
+        count += 1
+print(count)
