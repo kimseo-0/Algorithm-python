b5a1eebf48081a5d6989d9d84ec820dbc8ef7920
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-18 14:56:49 +0900

    add : greedy problem - BOG

---
+++ greedy/3-백준/1-11047.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+value_list = []
+for i in range(N):
+    value_list.insert(0, int(input()))
+
+count = 0
+for value in value_list:
+    if K >= value:
+        count += K // value
+        K = K % value
+
+print(count)
---
+++ greedy/3-백준/2-1931.py
@@ -0,0 +1,30 @@
+# https://velog.io/@yeseolee/Python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%A0%A5-%EC%A0%95%EB%A6%ACsys.stdin.readline
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+time_list = []
+for i in range(N):
+    start, end = map(int, input().split())
+    time_list.append([start, end])
+time_list.sort()
+
+select_list = [time_list[0]]
+for time in time_list[1:]:
+    # 해당 조건식에서 start == end 인 경우는 elif 로 넘어가도록 부등호를 잘 확인해야한다
+    # 결국 문제에 있는 조건을 잘 읽어라
+    if (select_list[-1][0] <= time[0]) and (time[1] < select_list[-1][1]):
+        select_list[-1] = time
+    elif (time[0] >= select_list[-1][1]) and (time[1] >= select_list[-1][1]):
+        select_list.append(time)
+
+print(len(select_list))
+
+# 위 문제를 해결하지 않을 경우 에러가 발생하는 예제
+# time_list = [[8, 9], [3, 7], [4, 5], [9, 10], [8, 8], [5, 6], [0, 1], [10, 10]]
+# time_list.sort()
+# print(time_list)
+
+# 다른 풀이 방법
+# end time을 기준으로 정렬을 한다
+# https://www.acmicpc.net/source/37732323
