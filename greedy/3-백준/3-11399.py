e4d0c6f28bc996b5caff241ec5e039ce64ea47e5
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-18 19:12:07 +0900

    update : greed solve problem
    2번째 방법

--- greedy/3-백준/3-11399.py
+++ greedy/3-백준/3-11399.py
@@ -1,3 +1,17 @@
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+# time_list = list(map(int, input().split()))
+# time_list.sort()
+#
+# min_sum = 0
+# for i in range(len(time_list)):
+#     min_sum += (N-i) * time_list[i]
+#
+# print(min_sum)
+
+# 2차 풀이
 import sys
 input = sys.stdin.readline
 
@@ -5,8 +19,10 @@ N = int(input())
 time_list = list(map(int, input().split()))
 time_list.sort()
 
-min_sum = 0
-for i in range(len(time_list)):
-    min_sum += (N-i) * time_list[i]
+prev_time = time_list[0]
+min_sum = prev_time
+for i in range(1, N):
+    prev_time += time_list[i]
+    min_sum += prev_time
 
 print(min_sum)
