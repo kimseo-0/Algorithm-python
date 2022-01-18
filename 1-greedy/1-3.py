ad2228d99c198396d6596d3d0a1837a58a04201b
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-18 11:46:00 +0900

    update : annotation and 2nd solve problem
    주석 추가
    해설을 참고하여 추가적인 풀이 방법 정리

--- 1-greedy/1-3.py
+++ 1-greedy/1-3.py
@@ -1,11 +1,23 @@
+# 1차 풀이 - 정답
 N, M = map(int, input().split())
 min_card_list = []
 
 for i in range(N):
     card_row = list(map(int, input().split()))
-    min_card_list.append(min(card_row))
+    min_card_list.append(min(card_row))     # 행에서 가장 작은 숫자의 카드만을 추가
 
-print(max(min_card_list))
+print(max(min_card_list))   # 가장 작은 숫자의 카드들 중 가장 큰 카드 찾기
+
+# 2차 풀이 - 정답(책 참고)
+N, M = map(int, input().split())
+
+result = 0
+for i in range(N):
+    card_row = list(map(int, input().split()))
+
+    result = max(result, min(card_row))
+
+print(result)   # 가장 작은 숫자의 카드들 중 가장 큰 카드 찾기
 
 '''
 ex1
