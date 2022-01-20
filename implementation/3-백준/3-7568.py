0c65467f97bd8d201177cbf405c77805c0a71063
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-20 19:54:03 +0900

    update : solving problem

--- implementation/3-백준/3-7568.py
+++ implementation/3-백준/3-7568.py
@@ -9,43 +9,17 @@ for i in range(N):
     people_row.append(i)
     people.append(people_row)
 
-people.sort(reverse=True)
+for i in range(len(people)):
+    count = 1
+    for j in range(len(people)):
+        if i == j:
+            continue
+        elif (people[i][0] < people[j][0]) and (people[i][1] < people[j][1]):
+            count += 1
 
-rank = [0] * N
-rank[people[0][2]] = 1
-prev_rank = 1
-for i in range(1, len(people)):
-    if people[i-1][0] == people[i][1]:
-        if people[i - 1][1] > people[i][1]:
-            prev_rank = i + 1
-    else:
-        if people[i-1][1] >= people[i][1]:
-            prev_rank = i + 1
-    rank[people[i][2]] = prev_rank
+    print(count, end=" ")
 
-for i in rank:
-    if i == N - 1:
-        print(i)
-    else:
-        print(i, end=" ")
-
-# import sys
-#
-# input = sys.stdin.readline
-#
-# N = int(input())
-# people = []
-#
-# for i in range(N):
-#     people_row = list(map(int, input().split()))
-#     people_row.append(i)
-#     people.append(people_row)
-#
-# for i in range(len(people)):
-#     count = 1
-#     for j in range(len(people)):
-#         if i == j:
-#             continue
-#         if (people[i][0] < people[j][0]) and (people[i][1] < people[i][j])
-#
-#     print(count, end=" ")
+# 이 문제를 5번이나 시도 했는데
+# 이유는 문제 조건상
+# a의 몸무게 < b의 몸무게 and a의 키 < b의 키 일때 b가 a보다 덩치가 크다라고
+# 제시되어있음에도 불구하고 a와 b의 몸무게 또는 키가 같을 때를 고려 했다.
