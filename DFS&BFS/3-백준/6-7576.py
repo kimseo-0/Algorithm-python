1ad1c8e892063e60e3a279e3990e47a702afdd57
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-01-24 09:41:41 +0900

    update : BOG problem

--- DFS&BFS/3-백준/6-7576.py
+++ DFS&BFS/3-백준/6-7576.py
@@ -3,6 +3,10 @@ from collections import deque
 
 input = sys.stdin.readline
 
+def print_box(graph):
+    for row in graph:
+        print(row)
+
 def BFS(graph, x, y):
     queue = deque([(x, y)])
     dx = [-1, 1, 0, 0]
@@ -19,13 +23,29 @@ def BFS(graph, x, y):
         for d in range(4):
             nx = v[0] + dx[d]
             ny = v[1] + dy[d]
-            if nx <= -1 or nx >= M or ny <= -1 or ny >= N or graph[nx][ny] == -1:
+            if nx <= -1 or nx >= M or ny <= -1 or ny >= N or graph[nx][ny] == -1 or graph[nx][ny] == 1:
                 continue
             if (1 <= graph[nx][ny]) and (graph[nx][ny] <= count):
                 continue
             if graph[nx][ny] == 0 or graph[nx][ny] > count:
                 graph[nx][ny] = count
                 queue.append((nx, ny))
+
+def DFS(graph, x, y, count):
+    if x <= -1 or x >= M or y <= -1 or y >= N or graph[x][y] == -1:
+        return
+    if count != 1 and (1 <= graph[x][y]) and (graph[x][y] <= count):
+        return
+    print(x, y, count)
+    print_box(graph)
+
+    graph[x][y] = count
+
+    DFS(graph, x + 1, y, count + 1)
+    DFS(graph, x - 1, y, count + 1)
+    DFS(graph, x, y + 1, count + 1)
+    DFS(graph, x, y - 1, count + 1)
+    return True
 
 
 N, M = map(int, input().split())
@@ -40,7 +60,8 @@ for num in range(count_good_tomato):
     for i in range(M):
         for j in range(N):
             if tomato_box[i][j] == 1:
-                BFS(tomato_box, i, j)
+                # BFS(tomato_box, i, j)
+                DFS(tomato_box, i, j, 1)
                 # print(tomato_box)
 
 isAvail = 1
