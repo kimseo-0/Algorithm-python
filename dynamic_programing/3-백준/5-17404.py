a471c1b574e7514a2e0353ad037647398380ffd2
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-02 08:36:49 +0900

    Update solved DP BOG problem

--- dynamic_programing/3-백준/5-17404.py
+++ dynamic_programing/3-백준/5-17404.py
@@ -1,81 +1,55 @@
+import heapq
 import sys
 input = sys.stdin.readline
+INF = int(1e9)
+
+def dijkstra(start, start_cost, distance):
+    q = []
+    heapq.heappush(q, (start_cost, start))
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now[0]][now[1]] < dist:
+            continue
+
+        for i in graph[now[0]][now[1]]:
+            cost = dist + i[1]
+            if distance[i[0][0]][i[0][1]] > cost:
+                distance[i[0][0]][i[0][1]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
 
 N = int(input())
-value_list = []
-for i in range(N):
-    value_list.append(list(map(int, input().split())))
+graph = [[[] for _ in range(N)] for _ in range(3)]
 
-DP_R = [0] * N
-DP_G = [0] * N
-DP_B = [0] * N
+start_R, start_G, start_B = map(int, input().split())
+for i in range(0, N - 1):
+    R, G, B = map(int, input().split())
+    graph[0][i].append(((1, i + 1), G))
+    graph[0][i].append(((2, i + 1), B))
+    graph[1][i].append(((2, i + 1), B))
+    graph[1][i].append(((0, i + 1), R))
+    graph[2][i].append(((0, i + 1), R))
+    graph[2][i].append(((1, i + 1), G))
 
-DP_R[1] = value_list[1][0]
-DP_G[1] = value_list[1][1]
-DP_B[1] = value_list[1][2]
+distance_R = [[INF] * N for _ in range(3)]
+distance_G = [[INF] * N for _ in range(3)]
+distance_B = [[INF] * N for _ in range(3)]
 
-pre_start_R = 0
-pre_start_G = 1
-pre_start_B = 2
-start_R = 0
-start_G = 1
-start_B = 2
+dijkstra((0, 0), start_R, distance_R)
+dijkstra((1, 0), start_G, distance_G)
+dijkstra((2, 0), start_B, distance_B)
 
-for i in range(2, N):
-    if value_list[i][0] + DP_G[i - 1] <= value_list[i][0] + DP_B[i - 1]:
-        DP_R[i] = value_list[i][0] + DP_G[i - 1]
-        start_R = pre_start_G
-    else:
-        DP_R[i] = value_list[i][0] + DP_B[i - 1]
-        start_R = pre_start_B
-    if value_list[i][1] + DP_B[i - 1] <= value_list[i][1] + DP_R[i - 1]:
-        DP_G[i] = value_list[i][1] + DP_B[i - 1]
-        start_G = pre_start_B
-    else:
-        DP_G[i] = value_list[i][1] + DP_R[i - 1]
-        start_G = pre_start_R
-    if value_list[i][2] + DP_R[i - 1] <= value_list[i][2] + DP_G[i - 1]:
-        DP_B[i] = value_list[i][2] + DP_R[i - 1]
-        start_B = pre_start_R
-    else:
-        DP_B[i] = value_list[i][2] + DP_G[i - 1]
-        start_B = pre_start_G
+# print(graph)
+# print(distance_R)
+# print(distance_G)
+# print(distance_B)
 
-    pre_start_R = start_R
-    pre_start_G = start_G
-    pre_start_B = start_B
-
-result = []
-
-if start_R == 0:
-    result.append(DP_R[N - 1] + value_list[0][1])
-    result.append(DP_R[N - 1] + value_list[0][2])
-elif start_R == 1:
-    result.append(DP_R[N - 1] + value_list[0][2])
-else:
-    result.append(DP_R[N - 1] + value_list[0][1])
-
-if start_G == 1:
-    result.append(DP_G[N - 1] + value_list[0][0])
-    result.append(DP_G[N - 1] + value_list[0][2])
-elif start_G == 0:
-    result.append(DP_G[N - 1] + value_list[0][2])
-else:
-    result.append(DP_G[N - 1] + value_list[0][0])
-
-if start_B == 2:
-    result.append(DP_B[N - 1] + value_list[0][0])
-    result.append(DP_B[N - 1] + value_list[0][1])
-elif start_B == 1:
-    result.append(DP_B[N - 1] + value_list[0][0])
-else:
-    result.append(DP_B[N - 1] + value_list[0][1])
-
-print(DP_R)
-print(DP_G)
-print(DP_B)
-
-print(min(result))
+print(min(distance_R[1][N - 1], distance_R[2][N - 1],
+          distance_G[0][N - 1], distance_G[2][N - 1],
+          distance_B[0][N - 1], distance_B[1][N - 1]))
 
 # https://www.acmicpc.net/problem/1149 와 연결되는 문제
 # https://www.acmicpc.net/problem/17404
