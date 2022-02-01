39dda19db1a0ff75cd797817696a57bac5003f13
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-02 08:37:08 +0900

    Update solved shortest route problem

---
+++ shortest_route/1-예제/9-5.py
@@ -0,0 +1,42 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+N, M, C = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+for i in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+distance = [INF] * (N + 1)
+dijkstra(C)
+
+count = 0
+max_distance = 0
+for i in range(2, N + 1):
+    if distance[i] != INF:
+        count += 1
+        max_distance = max(max_distance, distance[i])
+
+print(count, end=' ')
+print(max_distance)
+print(distance)
