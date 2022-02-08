d2ecc0a655e12978b9586f8c913d18660f232bfd
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-08 11:30:06 +0900

    Fixed syntax error

--- sort/3-백준/1-2750.py
+++ sort/3-백준/1-2750.py
@@ -1,25 +1,25 @@
 def insert_sort(array, N):
-  for i in range(1, N):
-    for j in range(i - 1, -1, j--):
-      if array[i - 1] > array[i]:
-        array[i], array[j] = array[j], array[i]
-        
-  
+    for i in range(1, N):
+        for j in range(i - 1, -1):
+            if array[i - 1] > array[i]:
+                array[i], array[j] = array[j], array[i]
+
+
 def select_sort(array, N):
-  for i in range(N - 1):
-    min_index = i
-    for j in range(i + 1, N):
-      if array[min_index] > array[j]:
-        min_index = j
-    array[i], array[min_index] = array[min_index], array[i]
+    for i in range(N - 1):
+        min_index = i
+        for j in range(i + 1, N):
+            if array[min_index] > array[j]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+
 
 N = int(input())
 num_list = []
 for i in range(N):
-  num_list.append(int(input()))
+    num_list.append(int(input()))
 
-  
-#   
+#
 # num_list.sort()
 
 # for i in range(N):
