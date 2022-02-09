913d7fd5a29384636f07587bee7c3eb8ab849a9d
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-09 11:26:23 +0900

    Create 27.py

---
+++ binary_search/27.py
@@ -0,0 +1,42 @@
+def binary_search_first(array, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == X:
+        if mid == 0 or array[mid - 1] < X:
+            return mid
+        else:
+            return binary_search_first(array, start, mid - 1)
+    elif array[mid] < X:
+        return binary_search_first(array, mid + 1, end)
+    else:
+        return binary_search_first(array, start, mid - 1)
+
+def binary_search_last(array, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == X:
+        if mid == N - 1 or array[mid + 1] > X:
+            return mid
+        else:
+            return binary_search_last(array, mid + 1, end)
+    elif array[mid] < X:
+        return binary_search_last(array, mid + 1, end)
+    else:
+        return binary_search_last(array, start, mid - 1)
+
+
+N, X = map(int, input().split())
+num_list = list(map(int, input().split()))
+start = 0
+end = N - 1
+start = binary_search_first(num_list, start, end)
+if start is None:
+    print(0)
+else:
+    end = binary_search_last(num_list, start, end)
+    print(end - start + 1)
+
+# 7 2
+# 1 1 2 2 2 2 3
