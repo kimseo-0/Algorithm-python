6391a249124f0432a6f116e60c3b23c6c28a3e1d
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-27 12:19:57 +0900

    Update 10.py

--- 이것이취업을위한코딩테스트다/part3-기출/2-implementation/10.py
+++ 이것이취업을위한코딩테스트다/part3-기출/2-implementation/10.py
@@ -1,65 +1,65 @@
 import sys
 input = sys.stdin.readline
 
-def rotation_key(key):
+
+def rotation_key(key, key_len):
     new_key = []
-    for i in range(len(key)):
+    for i in range(key_len):
         row = []
-        for j in range(len(key)):
+        for j in range(key_len):
             row.insert(0, key[j][i])
-        print(row)
         new_key.append(row)
     return new_key
 
-def padding_key(key, M, N):
-    for i in range(M):
-        key[i] = [*([0]*(N - 1)), *key[i], *([0]*(N - 1))]
-    for i in range(N - 1):
-        key.insert(0, [0] * (M + (N - 1) * 2))
-        key.append([0] * (M + (N - 1) * 2))
+def padding_key(key, key_len, lock_len):
+    for i in range(key_len):
+        key[i] = [*([0]*(lock_len - 1)), *key[i], *([0]*(lock_len - 1))]
+    for i in range(lock_len - 1):
+        key.insert(0, [0] * (key_len + (lock_len - 1) * 2))
+        key.append([0] * (key_len + (lock_len - 1) * 2))
 
 def solution(key, lock):
-    answer = True
+    key_length = len(key)
+    lock_length = len(lock)
 
-    M = len(key)
-    N = len(lock)
-
-    padding_key(key, M, N)
+    padding_key(key, key_length, lock_length)
 
     flag = True
+    # 4가지 종류의 key
     for r in range(4):
-        for mx in range(M + N - 1):
-            for my in range(M + N - 1):
-                flag = True
-                print(mx, my)
-                for i in range(N):
-                    for j in range(N):
-                        print((i, j), (i + mx, j + my), lock[i][j] != key[i + mx][j + my])
 
+        # key 를 (mx, my) 만큼 이동
+        for mx in range(key_length + lock_length - 1):
+            for my in range(key_length + lock_length - 1):
+
+                # key 와 lock 확인
+                flag = True
+                for i in range(lock_length):
+                    for j in range(lock_length):
+                        # 하나라도 맞지 않는다면 break
                         if lock[i][j] == key[i + mx][j + my]:
                             flag = False
                             break
-
+                    # 하나라도 맞지 않는 상황이면 break
                     if not flag:
                         break
 
+                # 모두 맞는 경우 더이상 key 를 확인할 필요가 없음
                 if flag:
                     break
+            # 모두 맞는 경우 더이상 key 를 확인할 필요가 없음
             if flag:
                 break
+        # 모두 맞는 경우 더이상 key 를 확인할 필요가 없음
         if flag:
             break
 
-        key = rotation_key(key)
+        key = rotation_key(key, key_length)
 
-    answer = flag
-    return answer
+    return flag
 
 
-key = [[0,0,0],[1,0,0],[0,1,1]]
-lock = [[1,1,1],[1,1,0],[1,0,1]]
+answer_key = [[0, 0, 0], [1, 0, 0], [0,  1, 1]]
+answer_lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
 
-for k in key:
-    print(k)
-# rotation_key(key)
-solution(key, lock)
+solution(answer_key, answer_lock)
