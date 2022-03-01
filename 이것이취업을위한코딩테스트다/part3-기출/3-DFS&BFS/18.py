51ebcedba1779701376e562771ccdb51d2d66830
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-01 12:22:28 +0900

    Create 18.py

---
+++ 이것이취업을위한코딩테스트다/part3-기출/3-DFS&BFS/18.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+def check_balance_index(p):
+    left = 0
+    right = 0
+    good = True
+    for i in range(len(p)):
+        if p[i] == "(":
+            left += 1
+        else:
+            right += 1
+        if right > left:
+            good = False
+        if left == right:
+            return i, good
+
+def solution(p):
+    result = ''
+
+    if p == "":
+        return result
+
+    index, good_bracket = check_balance_index(p)
+    u = p[:index + 1]
+    v = p[index + 1:]
+
+    v = solution(v)
+
+    if good_bracket:
+        return u + v
+    else:
+        result = "(" + v + ")"
+        for i in range(1, len(u) - 1):
+            if u[i] == ")":
+                result += "("
+            else:
+                result += ")"
+
+    return result
+
+
+print('"' + solution("()") + '"')
