d5241ccea9f919332c18665c11d975da3a19a081
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-02 12:44:25 +0900

    Create dijkstra.py

---
+++ Library/dijkstra.py
@@ -0,0 +1,17 @@
+import heapq
+
+def dijkstra(graph, distance, start):
+    q = []
+    heapq.heappush(q, (0, start))
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
