f428d81b9f8b742f080cadd2f7782e30785a49f4
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-02 19:45:27 +0900

    Update comment

--- 이것이취업을위한코딩테스트다/part3-기출/9-2020_상반기_삼성전자/47.py
+++ 이것이취업을위한코딩테스트다/part3-기출/9-2020_상반기_삼성전자/47.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 
 input = sys.stdin.readline
 
+
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]   # 위 부터 시계 반대 방향
 dy = [0, -1, -1, -1, 0, 1, 1, 1]
 
@@ -57,49 +58,61 @@ def find_avail_fish(map_list, shark):
 def DFS(map_list, fishes, shark, shark_size):
     result = []
 
+    # 물고기 이동
     move_fishes(map_list, fishes)
+    # 상어가 이동 가능한 물고기 번호
     avail_fishes_num = find_avail_fish(map_list, shark)
 
+    # 더이상 상어가 이동 불가능 할 경우 종료
     if len(avail_fishes_num) == 0:
         return shark_size
 
     for fish_num in avail_fishes_num:
+        # 상어가 먹은 물고기에 따라
+        # 지도, 물고기 상태, 상어 상태, 총 먹은 물고기가 달라지므로 copy 가 필요하다.
         new_map_list = deepcopy(map_list)
         new_fishes = deepcopy(fishes)
         new_shark = deepcopy(shark)
         new_shark_size = shark_size
 
-        [_, _, x, y] = new_shark
-        [num, _, nx, ny] = new_fishes[fish_num]   # 먹을 물고기 상태
-        new_map_list[x][y] = -1     # 상어 이동
-        new_shark = new_fishes[num]  # 상어 상태
-        new_shark[0] = 17
-        new_fishes[num] = -1  # 물고기 먹음
-        new_shark_size += (fish_num + 1)
-        new_map_list[nx][ny] = 17
+        # 상어 물고기 먹기 단계
+        [_, _, x, y] = new_shark                  # 상어 현재 상태
+        [num, _, nx, ny] = new_fishes[fish_num]   # 먹을 물고기 현재 상태
+        new_map_list[x][y] = -1                   # 상어 이동, 지도 빈칸 표기
+        new_shark = new_fishes[num]               # 이동한 상어 상태 초기화
+        new_shark[0] = 17                         # 상어 번호 초기화
+        new_fishes[num] = -1                      # 먹은 물고기 상태 초기화
+        new_shark_size += (fish_num + 1)          # 상어가 먹은 물고기 추가
+        new_map_list[nx][ny] = 17                 # 지도 상어 표기
 
+        # 상어 물고기 먹기 단계
         new_shark_size = DFS(new_map_list, new_fishes, new_shark, new_shark_size)
         result.append(new_shark_size)
 
+    # 결과 값들 중 가장 큰 값을 return
     return max(result)
 
 
+# 초기화
 base_map_list = []
 base_fishes = []     # 0 ~ 15번 물고기
 
 for i in range(N):
+    # 전체적으로 index 를 맞추기 위해서
+    # 물고기 번호와 물고기 방향을 -1 해서 처리함
     num1, dir1, num2, dir2, num3, dir3, num4, dir4 = map(lambda el: int(el) - 1, input().split())
     row = [(num1, dir1), (num2, dir2), (num3, dir3), (num4, dir4)]
     base_map_list.append([num1, num2, num3, num4])
     base_fishes.extend([[num1, dir1, i, 0], [num2, dir2, i, 1], [num3, dir3, i, 2], [num4, dir4, i, 3]])
 base_fishes.sort()
 
-base_shark_size = 0
+# 상어 초기화
+base_shark_size = 0   # 상어가 먹은 물고기 양
 base_shark = base_fishes[base_map_list[0][0]]  # 상어 상태
-base_shark[0] = 17
+base_shark[0] = 17  # 상어 번호 초기화
 base_fishes[base_map_list[0][0]] = -1     # 물고기 먹음
-base_shark_size += (base_map_list[0][0] + 1)
-base_map_list[0][0] = 17
+base_shark_size += (base_map_list[0][0] + 1)  # 상어가 먹을 물고기 추가
+base_map_list[0][0] = 17    # 지도 상어 표기
 
 print(DFS(base_map_list, base_fishes, base_shark, base_shark_size))
 
