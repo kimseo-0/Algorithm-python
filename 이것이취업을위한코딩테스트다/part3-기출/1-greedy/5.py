81eb90c8c23bdfbbe577140a98e3a7d331bd2737
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-05 11:34:45 +0900

    Create 5.py

---
+++ 이것이취업을위한코딩테스트다/part3-기출/1-greedy/5.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+ball_weight_list = list(map(int, input().split()))
+result = 0
+
+# solution 1
+# N*N/2
+# for i in range(N):
+#     for j in range(i + 1, N):
+#         if ball_weight_list[i] != ball_weight_list[j]:
+#             result += 1
+
+
+# solution 2 : part1 + part2 [0, 0, 2, 2] > 1 * (2 + 2) + 2 * (2)
+# part 1
+# count_list = [0] * (M + 1)
+# for i in range(N):
+#     count_list[ball_weight_list[i]] += 1
+
+# part 2
+# 해당 방법은 N(반복문) * N(sum 의 시간 복잡도)
+# for i in range(N):
+#     count_list[ball_weight_list[i]] -= 1
+#     result += (sum(count_list) - count_list[ball_weight_list[i]])
+
+# 이 방법은 M(반복문) * N(sum 의 시간 복잡도)
+# for i in range(1, M):
+#     count = count_list[i]
+#     result += (sum(count_list[i + 1:]) * count)
+
+
+# solution 3
+# N*logN
+ball_weight_list.sort()
+count = 1
+for i in range(N - 1):
+    if ball_weight_list[i] == ball_weight_list[i + 1]:
+        count += 1
+    else:
+        result += (N - i - 1) * count
+
+print(result)
+
+# 8 5
+# 1 5 4 3 2 4 5 2
+# > 25
