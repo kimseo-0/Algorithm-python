4d2463ffad2e90717c7a02f9f0208ff31feec525
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-16 10:01:34 +0900

    Add comment

--- 이것이취업을위한코딩테스트다/part3-기출/1-greedy/3.py
+++ 이것이취업을위한코딩테스트다/part3-기출/1-greedy/3.py
@@ -1,3 +1,7 @@
+# 문자열 뒤집기
+# 모든 숫자를 같게 하는 것이 목표
+# 전부 0으로 만드는 방법, 전부 1로 만드는 방법
+
 import sys
 input = sys.stdin.readline
 
@@ -11,7 +15,7 @@ else:
     one += 1
 
 for i in range(1, len(S)):
-    if S[i] != S[i-1]:
+    if S[i] != S[i - 1]:
         if S[i] == "0":
             zero += 1
         else:
@@ -20,11 +24,11 @@ for i in range(1, len(S)):
 print(min(zero, one))
 
 
-binary = [0, 0]
-binary[int(S[0])] += 1
-
-for i in range(1, len(S)):
-    if S[i] != S[i-1]:
-        binary[int(S[i])] += 1
-
-print(min(binary))
+# binary = [0, 0]
+# binary[int(S[0])] += 1
+#
+# for i in range(1, len(S)):
+#     if S[i] != S[i-1]:
+#         binary[int(S[i])] += 1
+#
+# print(min(binary))
