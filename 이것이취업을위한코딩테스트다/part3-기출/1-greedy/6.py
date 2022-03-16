6b2258230f11ee994b7b1e9ecdb6f907ae355c43
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-16 10:01:45 +0900

    Add comment

--- 이것이취업을위한코딩테스트다/part3-기출/1-greedy/6.py
+++ 이것이취업을위한코딩테스트다/part3-기출/1-greedy/6.py
@@ -1,8 +1,12 @@
+# 무지의 먹방 라이브
+# 그리디 기준 : 해당 음식을 먹는데 걸리는 시간이 짧은 순서대로
+
+# 우선순위큐가 필요할때, NlogN을 보장하며 오름차순 또는 내림차순 정렬이 피료한 경우 사용
 import heapq
 import sys
 input = sys.stdin.readline
 
-
+# 시간초과
 def solution(food_times, k):
     N = len(food_times)
     index = 0
@@ -18,6 +22,7 @@ def solution(food_times, k):
         index = (index + 1) % N
     return index
 
+# 시간초과
 def solution1(food_times, k):
     n = len(food_times)
     food_index = [i for i in range(n)]
@@ -39,7 +44,9 @@ def solution1(food_times, k):
 
     return food_index[index] + 1
 
+# 정답
 def solution2(food_times, k):
+    # 방송이 다시 시작한 후 다시 먹을 음식이 없는 경우 바로 종료
     if sum(food_times) <= k:
         return -1
 
@@ -47,10 +54,10 @@ def solution2(food_times, k):
     sort_food_times = []
     for i in range(n):
         heapq.heappush(sort_food_times, (food_times[i], i + 1))
-    sort_food_times.sort()
+    # sort_food_times.sort()
 
-    time = 0
-    previous_food_time = 0
+    time = 0    # 지금까지 먹은 음식들의 총 시간
+    previous_food_time = 0  # 바로 이전에 다 먹은 음식의 시간
     while True:
         if time + (sort_food_times[0][0] - previous_food_time) * n > k:
             break
@@ -58,13 +65,14 @@ def solution2(food_times, k):
         time += (food_time - previous_food_time) * n
         n -= 1
         previous_food_time = food_time
-        print(time)
 
+    # 남은 음식들을 번호 순서대로 다시 정렬
     sort_food_times.sort(key=lambda x: x[1])
 
+    # 남은 음식들 중 남은 시간만큼
     return sort_food_times[(k - time) % n][1]
 
 
-# print(solution([3, 1, 1], 5))
+print(solution2([3, 1, 2], 5))
 # print(solution1([3, 1, 1], 5))
-print(solution2([4, 3, 4, 1, 3], 9))
+# print(solution2([4, 3, 4, 1, 3], 9))
