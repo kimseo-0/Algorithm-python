dc121d17692e93cda1e19e7f941f724f64004f77
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-17 10:28:03 +0900

    Update 7.py

--- 이것이취업을위한코딩테스트다/part3-기출/2-implementation/7.py
+++ 이것이취업을위한코딩테스트다/part3-기출/2-implementation/7.py
@@ -1,10 +1,25 @@
+# 럭키 스트레이트
+# 절반 왼쪽의 자릿수의 합 == 절반 오른쪽 자릿수의 합 > LUCKY
+
 N = list(map(int, list(input())))
 
-mid = len(N) // 2
-left = sum(N[:mid])
-right = sum(N[mid:])
+# solution 1
+# mid = len(N) // 2
+# left = sum(N[:mid])
+# right = sum(N[mid:])
+#
+# if left == right:
+#     print("LUCKY")
+# else:
+#     print("READY")
 
-if left == right:
+# solution 2
+mid = len(N) // 2
+result = sum(N[:mid]) - sum(N[mid:])
+
+if result == 0:
     print("LUCKY")
 else:
     print("READY")
+
+
