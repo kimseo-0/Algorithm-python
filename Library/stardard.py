7ddb6675c36c7fa23b38aeeeccc7c2040187283b
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-17 10:27:41 +0900

    Create stardard.py

---
+++ Library/stardard.py
@@ -0,0 +1,15 @@
+# https://www.programiz.com/python-programming/methods/string/isnumeric
+data_string = '1'
+
+# 문자
+print('is alphabet? :', data_string.isalpha())
+# 숫자
+print('is number? :', data_string.isalnum())
+# 슷자
+print('is numeric? :', data_string.isnumeric())
+# 10진수
+print('is decimal? :', data_string.isdecimal())
+# 소문자
+print('is lower? :', data_string.islower())
+# 대문자
+print('is upper? :', data_string.isupper())
