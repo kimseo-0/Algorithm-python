68225a464f519573e45ece259260175e83176ada
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-18 10:29:55 +0900

    Update 19.py

--- 이것이취업을위한코딩테스트다/part3-기출/3-DFS&BFS/19.py
+++ 이것이취업을위한코딩테스트다/part3-기출/3-DFS&BFS/19.py
@@ -26,22 +26,28 @@ def min_calculation(index, operation, calculation_result, add, sub, mul, div):
     if (add + sub + mul + div) == 0:
         return calculation_result
 
-    result = []
-    for i in range(4):
-        result = []
-        # 언제 값들을 복사할지! 주의하자!
-        # 리스트, 딕셔너리와 같은 뎁스가 있는 데이터가 아니면 카피를 할 필요가 없다.
-        # 이 문제의 경우 new_operation_count 의 경우도 길이가 4이고 각각이 더하기, 빼기, 곱하기, 나누기를 의미하는 것이 결정되어있으므로
-        # add, sub, mul, div 라는 네 변수로 표현한다면 copy 를 할 필요성이 사라진다.
-        # new_index = index + 1
-        # new_calculation_result = calculation_result
-        # new_operation_count = deepcopy(operation_count)
-        # print(new_index, new_calculation_result, new_operation_count)
-        # if new_operation_count[i] != 0:
-        #     result.append(min_calculation(index + 1, i, calculation_result, new_operation_count))
+    result = 10e9
+    # 언제 값들을 복사할지! 주의하자!
+    # 리스트, 딕셔너리와 같은 뎁스가 있는 데이터가 아니면 카피를 할 필요가 없다.
+    # 이 문제의 경우 new_operation_count 의 경우도 길이가 4이고 각각이 더하기, 빼기, 곱하기, 나누기를 의미하는 것이 결정되어있으므로
+    # add, sub, mul, div 라는 네 변수로 표현한다면 copy 를 할 필요성이 사라진다.
+    # new_index = index + 1
+    # new_calculation_result = calculation_result
+    # new_operation_count = deepcopy(operation_count)
+    # print(new_index, new_calculation_result, new_operation_count)
+    # if new_operation_count[i] != 0:
+    #     result.append(min_calculation(index + 1, i, calculation_result, new_operation_count))
+    if add != 0:
+        result = min(result, min_calculation(index + 1, 0, calculation_result, add, sub, mul, div))
+    if sub != 0:
+        result = min(result, min_calculation(index + 1, 1, calculation_result, add, sub, mul, div))
+    if mul != 0:
+        result = min(result, min_calculation(index + 1, 2, calculation_result, add, sub, mul, div))
+    if div != 0:
+        result = min(result, min_calculation(index + 1, 3, calculation_result, add, sub, mul, div))
 
     # print(result)
-    return min(result)
+    return result
 
 
 def max_calculation(index, operation, calculation_result, operation_count):
@@ -88,6 +94,6 @@ operation_list = list(map(int, input().split()))
 answer = num_list[0]
 
 print(max_calculation(0, -1, answer, operation_list))
-print(min_calculation(0, -1, answer, operation_list, add, sub, mul, div))
+print(min_calculation(0, -1, answer, add, sub, mul, div))
 
 
