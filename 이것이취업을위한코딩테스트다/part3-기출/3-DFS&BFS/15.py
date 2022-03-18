c1928e6da85d25e9101f2cae1ca459bfa99cd0c4
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-18 10:29:28 +0900

    Update 15.py

--- 이것이취업을위한코딩테스트다/part3-기출/3-DFS&BFS/15.py
+++ 이것이취업을위한코딩테스트다/part3-기출/3-DFS&BFS/15.py
@@ -1,5 +1,12 @@
+# 특정 거리의 도시 찾기
+# 인접 리스트를 활용하여, 도시간의 거리를 계산하면 되는데,
+# BFS 를 활용하여 출발도시에서 가까운 도시부터 차례로 거리를 계산하면 된다.
+# 해당 문제가 BFS/DFS 문제로 분류되어있지만 사실 다익스트라 알고리즘으로 풀어도 된다.
+
 import heapq
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 INF = int(1e9)
 
@@ -23,6 +30,18 @@ def dijkstra(start, distance):
                 if cost > K:
                     return 0
 
+def BFS(start, distance):
+    q = deque([start])
+    distance[start] = 0
+
+    while q:
+        now = q.popleft()
+
+        for i in graph[now]:
+            if distance[i] == INF:
+                distance[i] = distance[now] + 1
+                q.append(i)
+
 
 N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
@@ -31,7 +50,8 @@ for i in range(M):
     a, b = map(int, input().split())
     graph[a].append((b, 1))
 
-dijkstra(X, distance)
+# dijkstra(X, distance)
+BFS(X, distance)
 
 no_city = True
 for i in range(1, N + 1):
