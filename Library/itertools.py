f2d53a09683e3976c5682c4689b1146d308f0a2c
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-19 23:26:37 +0900

    Update itertools.py

--- Library/itertools.py
+++ Library/itertools.py
@@ -1,6 +1,20 @@
 from itertools import combinations
 
+# combination
 data_list = [1, 2, 3, 4, 5, 6]
 N = 2
 combination_list = list(combinations(data_list, N))
 print('combination : ', combination_list)
+
+# sort
+data_list = [3, 2, 4, 6, 1, 5]
+data_list.sort()
+print(data_list)
+data_list.sort(reverse=True)
+print(data_list)
+data_list.sort(key=lambda x: -x)
+print(data_list)
+
+data_list = [(3, 'c'), (2, 'b'), (4, 'a'), (3, 'a'), (1, 'c'), (5, 'b')]
+data_list.sort(key=lambda x: (x[1], x[0]))
+print(data_list)
