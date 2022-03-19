d51e7a680e5797879c8df69ba14b8710eb8f903b
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-19 23:27:40 +0900

    Update 25.py

--- 이것이취업을위한코딩테스트다/part3-기출/4-sort/25.py
+++ 이것이취업을위한코딩테스트다/part3-기출/4-sort/25.py
@@ -1,3 +1,5 @@
+# 실패율
+
 def solution(N, stages):
     users = [0] * (N + 2)
     for i in range(len(stages)):
@@ -7,21 +9,34 @@ def solution(N, stages):
     answer.sort(key=lambda x: -(users[x]/sum(users[x:])) if sum(users[x:]) != 0 else 0)
     return answer
 
+def solution4(N, stages):
+    result = [(0, i) for i in range(1, N + 1)]
+    people = len(stages)
+
+    stages.sort()
+    count = 1
+    prev = stages[0]
+    for i in range(1, people):
+        if stages[i] == prev:
+            count += 1
+        else:
+            result[prev - 1] = (count / people, prev)
+            people -= count
+            prev = stages[i]
+            count = 1
+
+            if prev > N:
+                break
+
+    result.sort(key=lambda x: (-x[0], x[1]))
+    # return list(map(lambda x: x[1], result))
+    return [i[1] for i in result]
+
 
 # stages : [2, 1, 2, 6, 2, 4, 3, 3]
 # users  : [0, 1, 3, 2, 1, 0, 1]
-# solution(5, [2, 1, 2, 6, 2, 4, 3, 3])
-
-
-
-
-
-
-
-
-
-
-
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution4(5, [2, 1, 2, 6, 2, 4, 3, 3]))
 
 
 def solution2(N, stages):
@@ -37,8 +52,9 @@ def solution2(N, stages):
     answer.sort(key=lambda x: -result[x])
     return answer
 
-solution2(5, [2, 1, 2, 6, 2, 4, 3, 3])
 
+# solution2(5, [2, 1, 2, 6, 2, 4, 3, 3])
+# 답지 풀이방법
 def solution3(N, stages):
     result = {}
     all_challenger = len(stages)
@@ -51,4 +67,5 @@ def solution3(N, stages):
             result[stage] = 0
     return sorted(result, key=lambda x: result[x], reverse=True)
 
-print(solution3(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+
+# print(solution3(5, [2, 1, 2, 6, 2, 4, 3, 3]))
