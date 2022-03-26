07d437d5b8c83da4f7f9f15cc4b8e0c879ce6ca8
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-03-26 10:13:47 +0900

    Update 32.py

--- 이것이취업을위한코딩테스트다/part3-기출/6-dynamic_programing/32.py
+++ 이것이취업을위한코딩테스트다/part3-기출/6-dynamic_programing/32.py
@@ -19,11 +19,11 @@ for i in range(N):
 # print(max(num_list[N - 1]))
 
 # col 양쪽에 패딩 추가한 방법
-# DP = [[0] * (N + 2) for _ in range(N)]
-# DP[0][1] = num_list[0][0]
-#
-# for i in range(1, N):
-#     for j in range(1, i + 2):
-#         DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - 1]) + num_list[i][j - 1]
-#
-# print(max(DP[N - 1]))
+DP = [[0] * (N + 2) for _ in range(N)]
+DP[0][1] = num_list[0][0]
+
+for i in range(1, N):
+    for j in range(1, i + 2):
+        DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - 1]) + num_list[i][j - 1]
+
+print(max(DP[N - 1]))
