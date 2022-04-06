b6d83419fee7d11a23bbca0e815dad5b7afdc655
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-04-07 00:17:15 +0900

    Create 2869.py

---
+++ 백준(BOG)/extra/2869.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+A, B, V = map(int, input().split())
+
+count = (V - A) // (A - B) + 1
+move_distance = (V - A) % (A - B)
+
+
+
+# print(count)
+# print(V - move_distance)
+
+while True:
+    if move_distance <= 0:
+        break
+    move_distance -= (A - B)
+    count += 1
+
+print(count)
