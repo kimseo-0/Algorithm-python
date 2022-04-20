47bf7708935962c6a6b0e98c12f6e8ca3e5710ee
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-04-20 23:50:50 +0900

    Update [Programmers] 빛의 경로 Solved

--- 프로그래머스/Level2/86052.py
+++ 프로그래머스/Level2/86052.py
@@ -11,45 +11,36 @@ def solution(grid):
 
     edges = [[[1 for _ in range(4)] for _ in range(col)] for _ in range(row)]
 
-    while True:
-        x, y, d = -1, -1, -1
-        flag = False
-        for i in range(row):
-            for j in range(col):
-                for k in range(4):
-                    if edges[i][j][k] == 1:
-                        flag = True
-                        x, y, d = i, j, k
+    for i in range(row):
+        for j in range(col):
+            for k in range(4):
+                if edges[i][j][k] == 0:
+                    continue
+
+                x, y, d = i, j, k
+
+                count = 0
+                nx, ny, nd = x, y, d
+                while True:
+                    # print(count, nx, ny, nd)
+                    nx = (nx + dx[nd]) % row
+                    ny = (ny + dy[nd]) % col
+                    count += 1
+
+                    if grid[nx][ny] == 'L':
+                        nd = (nd + 1) % 4
+                    if grid[nx][ny] == 'R':
+                        nd = (nd - 1) % 4
+
+                    edges[nx][ny][nd] = 0
+
+                    if nx == x and ny == y and nd == d:
+                        answer.append(count)
                         break
-                if flag:
-                    break
-            if flag:
-                break
-
-        if not flag:
-            break
-
-        count = 0
-        nx, ny, nd = x, y, d
-        while True:
-            # print(count, nx, ny, nd)
-            nx = (nx + dx[nd]) % row
-            ny = (ny + dy[nd]) % col
-            count += 1
-
-            if grid[nx][ny] == 'L':
-                nd = (nd + 1) % 4
-            if grid[nx][ny] == 'R':
-                nd = (nd - 1) % 4
-
-            edges[nx][ny][nd] = 0
-
-            if nx == x and ny == y and nd == d:
-                answer.append(count)
-                break
 
     answer.sort()
     # print(answer)
     return answer
+
 
 solution(["SL", "LR"])
