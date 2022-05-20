d9681f7e7fff8e7ebc93329c1d05d8b1a71ace4f
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-05-20 09:14:33 +0900

    Update comment

--- 프로그래머스/Level3/42583.py
+++ 프로그래머스/Level3/42583.py
@@ -4,7 +4,13 @@ from collections import deque
 
 
 def solution(bridge_length, weight, truck_weights):
+
+    # 더이상 나갈 트럭이 없는 상황에서 남은 0 값들을 제거할 때 낭비되는 시간을 줄이기 위해
+    # 모든 트럭이 나갔는지 확인할 수 있는 남은 트럭 수 변수 생성
     left_truck_num = len(truck_weights)
+
+    # 파이썬 sum 을 사용하면 O(N) 이 걸리기 때문에
+    # 현재 다리위에 있는 모든 트럭의 무게의 합을 저장하는 변수 생성
     sum_of_weight = truck_weights[0]
     queue = [*[0] * (bridge_length - 1), truck_weights.pop(0)]
 
